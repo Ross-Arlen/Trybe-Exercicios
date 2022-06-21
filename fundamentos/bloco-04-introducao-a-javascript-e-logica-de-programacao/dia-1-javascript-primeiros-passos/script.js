8d3bb6467b384d80d455ed4c0be05f1e5fabd74e
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-21 18:01:24 -0300

    Exercício 6, movimentos das peças de xadrez e não case sensitive, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,13 +1,22 @@
-const a = 60;
-const b = 60;
-const c = 60;
-
-if (!(typeof a== typeof 1) || !(typeof b== typeof 1) || !(typeof c== typeof 1)) {
-    console.log("Error");
-} else if (a<0 || b<0 || c<0) {
-    console.log("Error");
-} else if (a+b+c==180) {
-        console.log(true);
-    } else {
-        console.log(false);
+let peça = 'REI';
+peça = peça.toLowerCase();
+switch (peça) {
+    case 'peão':
+    console.log('Se move para a frente, até duas casas no primeiro turno, ou só uma casa à partir daí. Ataca em diagonal, uma casa apenas.');
+    break;
+    case 'bispo':
+    console.log('Se move e ataca em diagonal. Quantas casas de distância for necessário.');
+    break;
+    case 'torre':
+    console.log('Se move e ataca em linha reta. Quantas casas de distância for necessário.');
+    break;
+    case 'cavalo':
+    console.log('Se move e ataca em L. Três casas de movimento ou no total.');
+    break;
+    case 'rainha':
+    console.log('Se move e ataca em qualquer direção. Quantas casas de distância for necessário.');
+    break;
+    case 'rei':
+    console.log('Se move e ataca em qualquer direção. Apenas uma casa de distância.');
+    break;
 }
