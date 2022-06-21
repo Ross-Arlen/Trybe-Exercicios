9cf790fceb07c0a9a74a09091c2335a31014b2ae
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-21 18:13:57 -0300

    Exercício 8, um entre três números é par, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,18 +1,8 @@
-let nota = 70;
-if (nota<50 && nota >=0) {
-    console.log('F');
-} else if (nota <0) {
-    console.log('Error');
-} else if (nota<60) {
-    console.log('E');
-} else if (nota<70) {
-    console.log('D');
-} else if (nota<80) {
-    console.log('C');
-} else if (nota<90) {
-    console.log('B');
-} else if (nota<100) {
-    console.log('A');
+const a = 10;
+const b = 11;
+const c = 12;
+if (a%2==0 || b%2==0 || c%2==0){
+    console.log(true);
 } else {
-    console.log('Error');
+    console.log(false);
 }
