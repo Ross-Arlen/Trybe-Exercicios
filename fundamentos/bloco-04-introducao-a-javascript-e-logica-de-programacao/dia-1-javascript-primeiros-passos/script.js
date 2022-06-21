0f5444360e3da4943323d1b6cac46005ccb216ea
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-21 17:47:56 -0300

    Exercício 5, ângulos internos do triângulo, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,9 +1,13 @@
-const a = 5;
+const a = 60;
+const b = 60;
+const c = 60;
 
-if (a>0) {
-    console.log("positive");
-} else if (a<0) {
-    console.log("negative");
-} else {
-    console.log("zero");
+if (!(typeof a== typeof 1) || !(typeof b== typeof 1) || !(typeof c== typeof 1)) {
+    console.log("Error");
+} else if (a<0 || b<0 || c<0) {
+    console.log("Error");
+} else if (a+b+c==180) {
+        console.log(true);
+    } else {
+        console.log(false);
 }
