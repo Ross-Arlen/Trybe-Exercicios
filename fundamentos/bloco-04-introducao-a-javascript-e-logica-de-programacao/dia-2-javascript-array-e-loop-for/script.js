77f8b7b670b288e0db4f472d325fc37daa3cc8fd
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-22 15:40:05 -0300

    Exercício 4, valor maior que 20 ou não, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -6,3 +6,8 @@ for (let index = 0; index < numbers.length; index += 1) {
 }
 let mean = sum/numbers.length;
 console.log(mean);
+if (mean>20) {
+    console.log('valor maior que 20');
+} else {
+    console.log('valor menor ou igual a 20');
+}
