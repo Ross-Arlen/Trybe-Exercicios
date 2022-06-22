c2610073025f3b0ae3e3639ae97a4260cfca131c
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-22 15:53:05 -0300

    Exercício 8, criar array de 1 a 25 com for loop, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,9 +1,6 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let smallest = numbers[0];
+let numbers = [];
 
-for (let index = 0; index < numbers.length; index += 1) {
-    if (smallest>numbers[index]) {
-        smallest = numbers[index];
-    }
+for (let index = 1; index < 26; index += 1) {
+    numbers.push(index)    
 }
-console.log(smallest);
+console.log(numbers);
