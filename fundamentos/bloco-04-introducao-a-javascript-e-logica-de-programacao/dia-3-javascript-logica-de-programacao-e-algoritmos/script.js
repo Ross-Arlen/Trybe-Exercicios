a241ab7367df527f68f2fba70ad51c3e52fd987b
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-23 16:36:55 -0300

    Exercício Bônus 1, imprimir quadrado de asteriscos do tamanho n, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,14 +1,10 @@
-let number;
+let n = 5;
 
-for (let index = 2; index <= 50; index += 1) {
-    let divisible = 0;
-    for (let index2 = 1; index2 <= index; index2 += 1) {
-        if (index%index2 == 0) {
-            divisible += 1;
+for (let index2 = 1; index2 <= n; index2 += 1) {
+    for (let index = 1; index <= n; index += 1) {
+        process.stdout.write('*');
+        if (index==n) {
+            process.stdout.write('\n');
         }
     }
-    if (divisible == 2) {
-        number = index;
-    }
 }
-console.log(number);
