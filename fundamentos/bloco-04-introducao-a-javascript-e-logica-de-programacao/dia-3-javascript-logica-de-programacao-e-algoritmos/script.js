72d557681b74250a74c31b71e17cf72e66920990
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-23 16:38:29 -0300

    Exercício Bônus 2, imprimir triângulo retângulo de asteriscos do tamanho n, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,9 +1,9 @@
 let n = 5;
 
 for (let index2 = 1; index2 <= n; index2 += 1) {
-    for (let index = 1; index <= n; index += 1) {
+    for (let index = 1; index <= index2; index += 1) {
         process.stdout.write('*');
-        if (index==n) {
+        if (index==index2) {
             process.stdout.write('\n');
         }
     }
