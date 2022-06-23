867d0deb9c32148ba88a8d0fefbc21c23865b506
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-23 16:21:35 -0300

    Exercício 4, maior número primo entre 2 e 50, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,9 +1,14 @@
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-let smaller = array[0];
+let number;
 
-for (let index = 0; index < array.length; index += 1) {
-    if (array[index].length < smaller.length) {
-        smaller = array[index];
+for (let index = 2; index <= 50; index += 1) {
+    let divisible = 0;
+    for (let index2 = 1; index2 <= index; index2 += 1) {
+        if (index%index2 == 0) {
+            divisible += 1;
+        }
+    }
+    if (divisible == 2) {
+        number = index;
     }
 }
-console.log(smaller);
+console.log(number);
