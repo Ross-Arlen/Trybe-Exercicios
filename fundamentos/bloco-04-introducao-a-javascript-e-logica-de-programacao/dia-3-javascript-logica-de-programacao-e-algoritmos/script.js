da7016aed7ba2aaaaf11e6c37c24a67999389be6
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-23 16:03:58 -0300

    Exercício 3 parte 2, menor palavra do array, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,9 +1,9 @@
 let array = ['java', 'javascript', 'python', 'html', 'css'];
-let bigger = '';
+let smaller = array[0];
 
 for (let index = 0; index < array.length; index += 1) {
-    if (array[index].length > bigger.length) {
-        bigger = array[index];
+    if (array[index].length < smaller.length) {
+        smaller = array[index];
     }
 }
-console.log(bigger);
+console.log(smaller);
