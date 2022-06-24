89ffc4320ddc3402b3ebfce9c0fa96f2303745e0
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-24 18:44:58 -0300

    Exercício 5, percorrer dois objetos e imprimir os valores de cada chave juntos, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-introducao-a-javascript-e-logica-de-programacao/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -4,8 +4,28 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
-  info['recorrente'] = 'Sim';
+  let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+  };
 
-  for (const key in info) {
-    console.log(info[key]);
+
+  info['recorrente'] = 'Sim';
+  info2['recorrente'] = 'Sim';
+
+  for (const key in info, info2) {
+    if (key === 'recorrente') {
+        if (info['recorrente'] === 'Sim' && info2['recorrente'] === 'Sim') {
+            console.log('Ambos recorrentes');   
+        } else if (info['recorrente'] != 'Sim' && info2['recorrente'] != 'Sim') {
+            console.log('Nenhum recorrente');
+        } else if (info['recorrente'] != 'Sim' && info2['recorrente'] === 'Sim') {
+            console.log('Apenas ' + info2.personagem + ' é recorrente');
+        } else if (info['recorrente'] === 'Sim' && info2['recorrente'] != 'Sim') {
+            console.log('Apenas ' + info.personagem + ' é recorrente');
+        }
+    } else {
+        console.log(info[key] + ' e ' + info2[key]);
+    }
   }
