b2ce72e8d4592774cd605b7907e02825189ce24b
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-24 15:02:05 -0300

    Exercício Bônus 3, inverter o lado do triângulo retângulo, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,6 +1,9 @@
 let n = 5;
 
 for (let index2 = 1; index2 <= n; index2 += 1) {
+    for (let index3 = 5; index3 >= index2; index3 -= 1) {
+        process.stdout.write(' ');
+    }
     for (let index = 1; index <= index2; index += 1) {
         process.stdout.write('*');
         if (index==index2) {
