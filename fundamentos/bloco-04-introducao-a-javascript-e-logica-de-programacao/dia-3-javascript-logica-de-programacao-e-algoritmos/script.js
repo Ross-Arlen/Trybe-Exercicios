93d7d74d133cd53026461a2818a7d7857f1a9d8f
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-24 15:56:46 -0300

    Exercício Bônus 6, detectar se o número fornecido é primo, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,18 +1,17 @@
 let n = 7;
+let prime = false;
 
-for (let index = 1; index <= n/2+1; index += 1) {
-    for (let index2 = 1; index2 < n-index; index2 += 1) {
-        process.stdout.write(' ');
+let divisible = 0;
+for (let index2 = 1; index2 <= n; index2 += 1) {
+    if (n%index2 == 0) {
+        divisible += 1;
     }
-    for (let index2 = 0; index2 < index*2-1; index2 += 1) {
-        if (index2 == 0 || index2 == index*2-2 || index >= n/2) {
-            process.stdout.write('*');
-        } else {
-            process.stdout.write(' ');
-        }
-    }
-    for (let index2 = 1; index2 < n-index; index2 += 1) {
-        process.stdout.write(' ');
-    }
-    process.stdout.write('\n');
+}
+if (divisible == 2) {
+    prime = true;
+}
+if (prime) {
+    console.log('O número é primo.');
+} else {
+    console.log('O número não é primo.');
 }
