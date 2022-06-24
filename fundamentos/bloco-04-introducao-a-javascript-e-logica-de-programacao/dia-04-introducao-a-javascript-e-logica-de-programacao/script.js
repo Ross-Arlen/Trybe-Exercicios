649ee360c1c1f2a447adc1b19228d0b64b0b4118
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-24 19:14:27 -0300

    Exercício 2 da Parte II, retorna índice do maior valor de um array, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-introducao-a-javascript-e-logica-de-programacao/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -1,4 +1,5 @@
 test = 'arara';
+testArray = [2, 3, 6, 7, 10, 1];
 
 function verificaPalindrome(string) {
     let test1 = string.split('');
@@ -11,4 +12,14 @@ function verificaPalindrome(string) {
     }
 }
 
-console.log(verificaPalindrome(test));
+function indexSmaller(array) {
+    let result = 0;
+    for (let index = 0; index < array.length; index += 1) {
+        const element = array[index];
+        if (element > result) {
+            result = index;
+        }
+    }
+    return result;
+}
+
