d374201e4683937501e821e3f6fbdaa20593b66f
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-24 19:15:17 -0300

    Exercício 2 da Parte II, consertado. Nome da função alterado.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-introducao-a-javascript-e-logica-de-programacao/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -12,7 +12,7 @@ function verificaPalindrome(string) {
     }
 }
 
-function indexSmaller(array) {
+function indexBigger(array) {
     let result = 0;
     for (let index = 0; index < array.length; index += 1) {
         const element = array[index];
