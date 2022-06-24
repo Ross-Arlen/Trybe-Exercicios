302df5766f0a269779005f9d7fea270e6fda4245
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-24 19:19:23 -0300

    Exercício 3 da Parte II, retorna índice do menor número no array, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-introducao-a-javascript-e-logica-de-programacao/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -1,5 +1,5 @@
 test = 'arara';
-testArray = [2, 3, 6, 7, 10, 1];
+testArray = [2, 4, 6, 7, 10, 0, -3];
 
 function verificaPalindrome(string) {
     let test1 = string.split('');
@@ -23,3 +23,15 @@ function indexBigger(array) {
     return result;
 }
 
+function indexSmaller(array) {
+    let result = array[0];
+    for (let index = 0; index < array.length; index += 1) {
+        const element = array[index];
+        if (element < result) {
+            result = index;
+        }
+    }
+    return result;
+}
+
+console.log(indexSmaller(testArray));
