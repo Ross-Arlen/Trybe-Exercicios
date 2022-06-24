9de07e447c35946b24da0726a8ce9055cbabfd49
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-24 15:48:28 -0300

    Exercício Bônus 5, pirâmide vazia com n asteriscos de base, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,11 +1,15 @@
-let n = 9;
+let n = 7;
 
 for (let index = 1; index <= n/2+1; index += 1) {
     for (let index2 = 1; index2 < n-index; index2 += 1) {
         process.stdout.write(' ');
     }
     for (let index2 = 0; index2 < index*2-1; index2 += 1) {
-        process.stdout.write('*');
+        if (index2 == 0 || index2 == index*2-2 || index >= n/2) {
+            process.stdout.write('*');
+        } else {
+            process.stdout.write(' ');
+        }
     }
     for (let index2 = 1; index2 < n-index; index2 += 1) {
         process.stdout.write(' ');
