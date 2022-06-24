3813e6e361ad0b17b0317602129b2b1733b40788
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-24 18:48:30 -0300

    Exercício 6, mostrar livro favorito do leitor com frase, concluído.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-introducao-a-javascript-e-logica-de-programacao/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -1,31 +1,14 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
   };
 
-  let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-  };
-
-
-  info['recorrente'] = 'Sim';
-  info2['recorrente'] = 'Sim';
-
-  for (const key in info, info2) {
-    if (key === 'recorrente') {
-        if (info['recorrente'] === 'Sim' && info2['recorrente'] === 'Sim') {
-            console.log('Ambos recorrentes');   
-        } else if (info['recorrente'] != 'Sim' && info2['recorrente'] != 'Sim') {
-            console.log('Nenhum recorrente');
-        } else if (info['recorrente'] != 'Sim' && info2['recorrente'] === 'Sim') {
-            console.log('Apenas ' + info2.personagem + ' é recorrente');
-        } else if (info['recorrente'] === 'Sim' && info2['recorrente'] != 'Sim') {
-            console.log('Apenas ' + info.personagem + ' é recorrente');
-        }
-    } else {
-        console.log(info[key] + ' e ' + info2[key]);
-    }
-  }
+  console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + "'O Pior Dia de Todos'");
