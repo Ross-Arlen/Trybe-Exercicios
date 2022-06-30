2085d51a47a42edf32255ba81c6cb4d4d9324b0a
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-30 16:09:38 -0300

    Exercício inicializado, e exercício 1 concluído.

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -0,0 +1,38 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+
+  function createDaysOfTheMonth() {
+    let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const daysList = document.querySelector('#days');
+  
+    for (let index = 0; index < decemberDaysList.length; index += 1) {
+      const days = decemberDaysList[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.classList.add('day');
+      dayListItem.innerText = days;
+      if (index - 1 === 24 || index - 1 === 25 || index - 1 === 31) {
+        dayListItem.classList.add('holiday');
+      }
+
+      if (index - 1 === 4 || index - 1 === 11 || index - 1 === 18 || index - 1 === 25) {
+        dayListItem.classList.add('friday');
+      }
+  
+      daysList.appendChild(dayListItem);
+    };
+  };
+
+  createDaysOfTheMonth();
