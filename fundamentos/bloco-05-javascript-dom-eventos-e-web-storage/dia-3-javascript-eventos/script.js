364d771b048f095168a4d620dc2d0c408239b727
Tiago Hasse <tiagoh_n@hotmail.com>
2022-06-30 19:00:10 -0300

    Exercício 6 concluído.

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -13,6 +13,13 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
+  function zoomIn(event) {
+    event.target.style.fontSize = '40px';
+  }
+
+  function zoomOut(event) {
+    event.target.style.fontSize = '20px';
+  }
 
   function createDaysOfTheMonth() {
     let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
@@ -32,6 +39,8 @@ function createDaysOfTheWeek() {
       }
   
       daysList.appendChild(dayListItem);
+      dayListItem.addEventListener('mouseenter', zoomIn);
+      dayListItem.addEventListener('mouseleave', zoomOut);
     };
   };
 
