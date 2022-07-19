a2a2927b8b1f5961b76b3d93b71307147f886d1d
rossarlen <tiagoh_n@hotmail.com>
2022-07-19 00:23:52 -0300

    Teste 1 do exercício 2 feito.

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/script.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/script.test.js
@@ -1,15 +1,23 @@
-const sum = require('./script');
+const { sum, myRemove } = require('./script');
 
-test('Tests if 4 + 5 = 9.', () => {
-    expect(sum(4, 5)).toBe(9)
-})
+describe('sum: ', () => {
+    test('Tests if 4 + 5 = 9.', () => {
+        expect(sum(4, 5)).toBe(9)
+    });
+    
+    test('Tests if 0 + 0 = 0.', () => {
+        expect(sum(0, 0)).toBe(0)
+    });
+    
+    test('Tests if error is thrown on string.', () => {
+        expect(() => {
+            sum(4, '5');
+        }).toThrow('parameters must be numbers')
+    });
+});
 
-test('Tests if 0 + 0 = 0.', () => {
-    expect(sum(0, 0)).toBe(0)
-})
-
-test('Tests if error is thrown on string.', () => {
-    expect(() => {
-        sum(4, '5');
-    }).toThrow('parameters must be numbers')
-})
+describe('myRemove: ', () => {
+    test('Tests if [1, 2, 3, 4], 3 returns [1, 2, 3]', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4])
+    });
+});
