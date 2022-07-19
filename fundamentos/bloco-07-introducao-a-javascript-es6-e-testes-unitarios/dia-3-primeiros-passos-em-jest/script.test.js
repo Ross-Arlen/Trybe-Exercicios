c4827d0ea34d3847af26c835ebef4d3b8e833cd8
rossarlen <tiagoh_n@hotmail.com>
2022-07-18 23:55:30 -0300

    Teste 3 e 4 do exercício 1 feito.

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/script.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/script.test.js
@@ -7,3 +7,9 @@ test('Tests if 4 + 5 = 9.', () => {
 test('Tests if 0 + 0 = 0.', () => {
     expect(sum(0, 0)).toBe(0)
 })
+
+test('Tests if error is thrown on string.', () => {
+    expect(() => {
+        sum(4, '5');
+    }).toThrow('parameters must be numbers')
+})
