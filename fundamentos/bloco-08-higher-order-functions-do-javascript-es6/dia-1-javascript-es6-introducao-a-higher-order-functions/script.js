e7741e79da410b8d1cd7f8b09ef0aeedc24a8b32
rossarlen <tiagoh_n@hotmail.com>
2022-07-21 16:04:05 -0300

    Exercício 3 feito.

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,16 +1,24 @@
-const sorteio = (number, func) => {
-    const randNumber = Math.round(Math.random() * 5);
-    if (func(number, randNumber)) {
-    return 'Parabéns você ganhou';
-    }
-    return 'Tente novamente';
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const score = (right, student, func) => {
+    return func(right, student);
 }
 
-const check = (number, randNumber) => {
-    if (number === randNumber) {
-        return true;
+const compare = (right, student) => {
+    let score = 0;
+    for (let index = 0; index < right.length; index += 1) {
+        const element = right[index];
+        const element2 = student[index];
+        if (element === element2) {
+            score += 1;
+        } else if (element2 === 'N.A') {
+            continue;
+        } else if (element !== element2) {
+            score -= 0.5;
+        }
     }
-    return false;
+    return score;
 }
 
-console.log(sorteio(3, check));
+console.log(score(RIGHT_ANSWERS, STUDENT_ANSWERS, compare));
